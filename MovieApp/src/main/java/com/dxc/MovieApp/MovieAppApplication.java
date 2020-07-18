a2c186abe65b4715537bf2173f8a8d8479adde85
Nidhin K James <nidhinkjames@gmail.com>
2020-07-18 12:08:06 +0530

    spring-maven updated

--- MovieApp/src/main/java/com/dxc/MovieApp/MovieAppApplication.java
+++ MovieApp/src/main/java/com/dxc/MovieApp/MovieAppApplication.java
@@ -2,11 +2,14 @@ package com.dxc.MovieApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class MovieAppApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(MovieAppApplication.class, args);
 	}
 
--- MovieApp/src/main/java/com/dxc/MovieApp/controller/MovieController.java
+++ MovieApp/src/main/java/com/dxc/MovieApp/controller/MovieController.java
@@ -14,50 +14,47 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dxc.MovieApp.entity.Movie;
 import com.dxc.MovieApp.service.MovieService;
 
+@RestController
+@RequestMapping("/movies")
 public class MovieController 
 {
-	@RestController
-	@RequestMapping("/movies")
-	public class EmployeeController 
-	{
 
-		@Autowired
-		MovieService movieService;
+	@Autowired
+	MovieService movieService;
 
-		// http://localhost:8562/movies/all
-		@RequestMapping(value = "/all", method = RequestMethod.GET)
-		public List<Movie> getAllMovies() {
-			return this.movieService.getAllMovies();
-		}
+	// http://localhost:8562/movies/all
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	public List<Movie> getAllMovies() {
+		return this.movieService.getAllMovies();
+	}
 
-		// http://localhost:8562/movies/addMovie
-		@RequestMapping(value = "/addMovie", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-		public Movie addMovie(@RequestBody Movie movie) {
-			return this.movieService.addMovie(movie);
-		}
+	// http://localhost:8562/movies/addMovie
+	@RequestMapping(value = "/addMovie", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Movie addMovie(@RequestBody Movie movie) {
+		return this.movieService.addMovie(movie);
+	}
 
-		// http://localhost:8562/movies/updateMovie
-		@RequestMapping(value = "/updateEmployee", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-		public Movie updateEmployee(@RequestBody Movie movie) {
-			return this.movieService.updateMovie(movie);
-		}
+	// http://localhost:8562/movies/updateMovie
+	@RequestMapping(value = "/updateEmployee", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Movie updateEmployee(@RequestBody Movie movie) {
+		return this.movieService.updateMovie(movie);
+	}
 
-		// http://localhost:8562/movies/1
-		@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-		public Optional<Movie> getEmployee(@PathVariable int id) {
-			return this.movieService.getMovieById(id);
-		}
+	// http://localhost:8562/movies/1
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Optional<Movie> getEmployee(@PathVariable int id) {
+		return this.movieService.getMovieById(id);
+	}
 
-		// http://localhost:8562/movies/all
-		@RequestMapping(value = "/all", method = RequestMethod.DELETE)
-		public void deleteAllMovies() {
-			this.movieService.deleteAllMovies();
-		}
+	// http://localhost:8562/movies/all
+	@RequestMapping(value = "/all", method = RequestMethod.DELETE)
+	public void deleteAllMovies() {
+		this.movieService.deleteAllMovies();
+	}
 
-		// http://localhost:8562/movies/1
-		@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-		public void deleteMovie(@PathVariable int id) {
-			this.movieService.deleteMovieById(id);
-		}
+	// http://localhost:8562/movies/1
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deleteMovie(@PathVariable int id) {
+		this.movieService.deleteMovieById(id);
 	}
 }
